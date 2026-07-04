50df4bdec2a50a52ac06bbb42a32a597caf5876b
CVBGOD <mercwar01@gmail.com>
2026-07-04 12:47:39 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -353,7 +353,7 @@ function enforceFrameRulesOnLoad() {
         // 🔥 Shift the .screen object left by 200px when in frame
         const screenEl = document.querySelector(".screen");
         if (screenEl) {
-            screenEl.style.left = " 20px";
+            screenEl.style.left = "calc(50% - 20px)";
             screenEl.style.transform = "translate(-50%, -50%)";
         }
 
